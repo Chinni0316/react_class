2fe0d24f86a7225449ce7584388298cb9954bf22
Chinni0316 <165983644+Chinni0316@users.noreply.github.com>
2024-07-09 22:06:06 +0530

    add file

---
+++ src/component/images/image.js
@@ -0,0 +1,9 @@
+const Image_profile=()=>{
+    return(
+        <div>
+        <img src="https://fakestoreapi.com/img/71kWymZ+c+L._AC_SX679_.jpg" alt="box" width={200} height={200}></img>
+        <p>This is one of the data</p>
+        </div>
+    )
+}
+export default Image_profile;
---
+++ src/component/tables/table.js
@@ -0,0 +1,33 @@
+import "./table.css"
+const Table_name=()=>{
+    return(
+        <div>
+            <tr>
+                <th>s.no</th>
+                <th>Name</th>
+            </tr>
+            <tr>
+                <td>01</td>
+                <td>Ashwini</td>
+            </tr>
+            <tr>
+                <td>02</td>
+                <td>Varshini</td>
+            </tr>
+            <tr>
+                <td>03</td>
+                <td>Ramya</td>
+            </tr>
+            <tr>
+                <td>04</td>
+                <td>Bhargavi</td>
+            </tr>
+            <tr>
+                <td>05</td>
+                <td>Pooja</td>
+            </tr>
+        </div>
+    )
+
+};
+export default Table_name;
