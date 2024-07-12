c489696ac9477aba4647ba2a082b689acb49767e
Chinni0316 <165983644+Chinni0316@users.noreply.github.com>
2024-07-12 23:19:34 +0530

    add file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import IplItem from './component/Iplmatch/Iplmatch';
+import CustomSpinner from './component/spinner/custom_spinner';
+import CustomWatch from './component/Watch/custom_watch';
 
 
 
@@ -8,7 +10,9 @@ import IplItem from './component/Iplmatch/Iplmatch';
 function App() {
   return (
     <div>
-    <IplItem></IplItem>
+      <CustomSpinner></CustomSpinner>
+      <CustomWatch></CustomWatch>
+    {/* <IplItem></IplItem> */}
    
   
    </div>
